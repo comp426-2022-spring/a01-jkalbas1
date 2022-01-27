060e07246986a78062537eec6c72c92d0722ccd3
Joey Kalbas <jkalbas@email.unc.edu>
2022-01-27 13:40:30 -0500

    retooled

--- server.js
+++ server.js
@@ -31,8 +31,7 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
 
     const server = http.createServer((req, res) => {
         res.statusCode = 200
-        res.setHeader = ('Content-Type', 'text/html')
-        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.setHeader('Content-Type', 'text/html')
         res.end(data)
     })
 
